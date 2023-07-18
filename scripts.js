7b42e3e77fa44da7cfe6434c5b6fa7822e415e3f
bjasperson <ben.jasperson@gmail.com>
2023-07-18 11:42:29 -0500

    added annotation, rectangle and log/log plot for fig4
    - updated svg size and circle radius
    - added rectangle and annotation
    - clarified webpage text, now fig4 is log/log

--- scripts.js
+++ scripts.js
@@ -7,8 +7,8 @@ async function svg1() {
     console.log(data);  
 
     const margin = 50;
-    const width = 200;
-    const height = 200;
+    const width = 400;
+    const height = 400;
     var x = d3.scaleLinear().domain([0,2]).range([0,width]);
     var y = d3.scaleLinear().domain([0,2500]).range([height, 0]);
 
@@ -17,7 +17,7 @@ async function svg1() {
         .selectAll('circle').data(data).enter().append('circle')
         .attr('cx',function(d) { return x(parseFloat(d.surface_energy_110_fcc_avg)) } )
         .attr('cy',function(d) { return y(parseFloat(d.c44_fcc_avg)) } )
-        .attr('r',function(d) { return 2.0 } );
+        .attr('r',function(d) { return 3.0 } );
 
 
     d3.select('#svg1').append('g')
@@ -39,8 +39,8 @@ async function svg2() {
     console.log(data);  
 
     const margin = 50;
-    const width = 200;
-    const height = 200;
+    const width = 400;
+    const height = 400;
     var x = d3.scaleLinear().domain([0,2]).range([0,width]);
     var y = d3.scaleLinear().domain([0,2500]).range([height, 0]);
 
@@ -49,10 +49,32 @@ async function svg2() {
         .selectAll('circle').data(data).enter().append('circle')
         .attr('cx',function(d) { return x(parseFloat(d.surface_energy_110_fcc)) } )
         .attr('cy',function(d) { return y(parseFloat(d.c44_fcc)) } )
-        .attr('r',function(d) { return 2.0 } )
+        .attr('r',function(d) { return 3.0 } )
         .append('title')
         .text(function(d) { return d.model } );
 
+    // add annotation
+    d3.select('#svg2').append('g')
+        .attr('transform', 'translate('+margin+','+margin+')')
+        .append("text")
+        .text("These results use the same")
+        .attr("x",x(0.9))
+        .attr("y",y(900.0));
+
+    d3.select('#svg2').append('g')
+        .attr('transform', 'translate('+margin+','+margin+')')
+        .append("text")
+        .text("interatomic model")
+        .attr("x",x(0.9))
+        .attr("y",y(800.00));
+
+    d3.select('#svg2').append('g')
+        .attr('transform', 'translate('+margin+','+margin+')')
+        .selectAll('rect').data(data).enter().append('rect')
+        .attr("width",300)
+        .attr("height",300)
+        .attr("x",x(0.5))
+        .attr("y",y(2600));
 
     d3.select('#svg2').append('g')
         .attr('transform','translate('+margin+','+margin+')')
@@ -72,8 +94,8 @@ async function svg3() {
     console.log(data);  
 
     const margin = 50;
-    const width = 200;
-    const height = 200;
+    const width = 400;
+    const height = 400;
     var x = d3.scaleLinear().domain([0,0.25]).range([0,width]);
     var y = d3.scaleLinear().domain([0,250]).range([height, 0]);
 
@@ -82,7 +104,7 @@ async function svg3() {
         .selectAll('circle').data(data).enter().append('circle')
         .attr('cx',function(d) { return x(parseFloat(d.surface_energy_110_fcc_avg)) } )
         .attr('cy',function(d) { return y(parseFloat(d.c44_fcc_avg)) } )
-        .attr('r',function(d) { return 2.0 } );
+        .attr('r',function(d) { return 3.0 } );
 
 
     d3.select('#svg3').append('g')
@@ -104,17 +126,17 @@ async function svg4() {
     console.log(data);  
 
     const margin = 50;
-    const width = 200;
-    const height = 200;
-    var x = d3.scaleLinear().domain([0,2]).range([0,width]);
-    var y = d3.scaleLinear().domain([0,600]).range([height, 0]);
+    const width = 400;
+    const height = 400;
+    var x = d3.scaleLog().domain([.0005,2]).range([0,width]);
+    var y = d3.scaleLog().domain([1,600]).range([height, 0]);
 
     d3.select('#svg4').append('g')
         .attr('transform', 'translate('+margin+','+margin+')')
         .selectAll('circle').data(data).enter().append('circle')
         .attr('cx',function(d) { return x(parseFloat(d.surface_energy_110_fcc)) } )
         .attr('cy',function(d) { return y(parseFloat(d.c44_fcc)) } )
-        .attr('r',function(d) { return 2.0 } )
+        .attr('r',function(d) { return 3.0 } )
         .append('title')
         .text(function(d) { return d.model } );
 
@@ -122,12 +144,12 @@ async function svg4() {
     d3.select('#svg4').append('g')
         .attr('transform','translate('+margin+','+margin+')')
         .call(d3.axisLeft(y));
-                    //.tickValues([10,20,50,100])
+                    //.tickValues([10,20,50,100]);
                     //.tickFormat(d3.format("~s")));
 
     d3.select('#svg4').append('g')
         .attr('transform','translate('+margin+','+(height+margin)+')')
-        .call(d3.axisBottom(x));
-                    //.tickValues([10,20,50,100])
+        .call(d3.axisBottom(x))
+                    //.tickValues([.001,.01,.1,1]);
                     //.tickFormat(d3.format("~s")));
 }
