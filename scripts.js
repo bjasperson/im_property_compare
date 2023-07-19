5c3d9d711ea9634fd46669a978cce8f646dfdd6c
bjasperson <ben.jasperson@gmail.com>
2023-07-19 14:39:33 -0500

    added explore page; button/load updated

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // move the data outside of the function and make it global once working
 
-async function svg1() {
+async function svg1a() {
     // const data = await d3.csv("https://benjasperson.com/im_property_compare/data.csv");
     d3.select("#svg1").selectAll("*").remove();
     const data = await d3.csv("./data.csv");
@@ -34,7 +34,7 @@ async function svg1() {
                     //.tickFormat(d3.format("~s")));
 }
 
-async function svg2() {
+async function svg1b() {
     //const data = await d3.csv("https://benjasperson.com/im_property_compare/data.csv");
     d3.select("#svg1").selectAll("*").remove();
 
@@ -92,7 +92,41 @@ async function svg2() {
                     //.tickFormat(d3.format("~s")));
 }
 
-async function svg3() {
+
+async function svg2a() {
+    d3.select("#svg2").selectAll("*").remove();
+    const data = await d3.csv("./data.csv");
+    console.log(data);  
+
+    const margin = 50;
+    const width = 400;
+    const height = 400;
+    var x = d3.scaleLinear().domain([0,0.25]).range([0,width]);
+    var y = d3.scaleLinear().domain([0,250]).range([height, 0]);
+
+    d3.select('#svg2').append('g')
+        .attr('transform', 'translate('+margin+','+margin+')')
+        .selectAll('circle').data(data).enter().append('circle')
+        .attr('cx',function(d) { return x(parseFloat(d.surface_energy_110_fcc_avg)) } )
+        .attr('cy',function(d) { return y(parseFloat(d.c44_fcc_avg)) } )
+        .attr('r',function(d) { return 3.0 } );
+
+
+    d3.select('#svg2').append('g')
+        .attr('transform','translate('+margin+','+margin+')')
+        .call(d3.axisLeft(y));
+                    //.tickValues([10,20,50,100])
+                    //.tickFormat(d3.format("~s")));
+
+    d3.select('#svg2').append('g')
+        .attr('transform','translate('+margin+','+(height+margin)+')')
+        .call(d3.axisBottom(x));
+                    //.tickValues([10,20,50,100])
+                    //.tickFormat(d3.format("~s")));
+}
+
+async function svg2b() {
+    d3.select("#svg2").selectAll("*").remove();
     const data = await d3.csv("./data_filtered.csv");
     console.log(data);  
 
@@ -102,7 +136,7 @@ async function svg3() {
     var x = d3.scaleLinear().domain([0,0.25]).range([0,width]);
     var y = d3.scaleLinear().domain([0,250]).range([height, 0]);
 
-    d3.select('#svg3').append('g')
+    d3.select('#svg2').append('g')
         .attr('transform', 'translate('+margin+','+margin+')')
         .selectAll('circle').data(data).enter().append('circle')
         .attr('cx',function(d) { return x(parseFloat(d.surface_energy_110_fcc_avg)) } )
@@ -110,13 +144,13 @@ async function svg3() {
         .attr('r',function(d) { return 3.0 } );
 
 
-    d3.select('#svg3').append('g')
+    d3.select('#svg2').append('g')
         .attr('transform','translate('+margin+','+margin+')')
         .call(d3.axisLeft(y));
                     //.tickValues([10,20,50,100])
                     //.tickFormat(d3.format("~s")));
 
-    d3.select('#svg3').append('g')
+    d3.select('#svg2').append('g')
         .attr('transform','translate('+margin+','+(height+margin)+')')
         .call(d3.axisBottom(x));
                     //.tickValues([10,20,50,100])
@@ -124,7 +158,7 @@ async function svg3() {
 }
 
 
-async function svg4() {
+async function svg3() {
     const data = await d3.csv("./data_filtered.csv");
     console.log(data);  
 
@@ -134,7 +168,7 @@ async function svg4() {
     var x = d3.scaleLog().domain([.0005,2]).range([0,width]);
     var y = d3.scaleLog().domain([1,600]).range([height, 0]);
 
-    d3.select('#svg4').append('g')
+    d3.select('#svg3').append('g')
         .attr('transform', 'translate('+margin+','+margin+')')
         .selectAll('circle').data(data).enter().append('circle')
         .attr('cx',function(d) { return x(parseFloat(d.surface_energy_110_fcc)) } )
@@ -144,13 +178,13 @@ async function svg4() {
         .text(function(d) { return d.model } );
 
 
-    d3.select('#svg4').append('g')
+    d3.select('#svg3').append('g')
         .attr('transform','translate('+margin+','+margin+')')
         .call(d3.axisLeft(y));
                     //.tickValues([10,20,50,100]);
                     //.tickFormat(d3.format("~s")));
 
-    d3.select('#svg4').append('g')
+    d3.select('#svg3').append('g')
         .attr('transform','translate('+margin+','+(height+margin)+')')
         .call(d3.axisBottom(x))
                     //.tickValues([.001,.01,.1,1]);
